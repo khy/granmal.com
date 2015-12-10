51784ba28709204f716ee8cb803127e675990733
Kevin Hyland <khy@me.com>
2015-12-09 22:37:07 -0500

    Further work on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,27 @@ const spawnSync = require('child_process').spawnSync
 const gulp = require('gulp')
 const util = require('gulp-util')
 const bump = require('gulp-bump')
+const git = require('gulp-git')
 
 function version() {
   return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version
 }
 
-gulp.task('bump-version', () => {
+gulp.task('set-release-version', () => {
   return gulp.src('./package.json')
     .pipe(bump({type: "patch"}).on('error', util.log))
     .pipe(gulp.dest('./'))
+})
+
+gulp.task('commit-release-version', () => {
+  return gulp.src('./package.json').
+    pipe(git.commit('Setting version to ' + version()))
+})
+
+gulp.task('tag-release-version', () => {
+  git.tag('v' + version(), 'Releasing ' + version(), (err) => {
+    if (err) throw err;
+  })
 })
 
 gulp.task('build-docker', () => {
