e274e20bb048e85645df94c8be705cf8d2e58b47
Kevin Hyland <khy@me.com>
2015-12-27 21:14:14 -0500

    Rudimentary layout for planned transactions page

--- apps/budget/client/components/PlannedTxns/index.jsx
+++ apps/budget/client/components/PlannedTxns/index.jsx
@@ -1,14 +1,66 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import _map from 'lodash/collection/map'
 
 import Navbar from '../Navbar'
 
 class PlannedTxns extends React.Component {
 
   render() {
+    const txnTypeOptions = (
+      _map(this.props.txnTypes, (txnType) => {
+        return <option value={txnType.guid} key={txnType.guid}>{txnType.name}</option>
+      })
+    )
+
+    const accountOptions = (
+      _map(this.props.accounts, (account) => {
+        return <option value={account.guid} key={account.guid}>{account.name}</option>
+      })
+    )
+
     return (
-      <div className="container">
-        <h1>Planned Transactions</h1>
+      <div>
+        <Navbar />
+
+        <div className="container">
+          <h1>Planned Transactions</h1>
+
+          <div className="card card-block">
+            <form>
+              <fieldset className="form-group">
+                <label>Transaction Type</label>
+                <select ref="txnTypeGuidSelect" className="form-control c-select">
+                  {txnTypeOptions}
+                </select>
+              </fieldset>
+
+              <fieldset className="form-group">
+                <label>Account</label>
+                <select ref="accountGuidSelect" className="form-control c-select">
+                  {accountOptions}
+                </select>
+              </fieldset>
+
+              <button type="submit" className="btn btn-primary">Filter</button>
+            </form>
+          </div>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Type</th>
+                <th>Account</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
