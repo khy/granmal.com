f0c32080f8731d669cb7edd8c0c5f7922d8e795b
Kevin Hyland <khy@me.com>
2016-02-02 22:35:40 -0500

    Get rid of api.js nonsense

--- apps/budget/server/app.js
+++ apps/budget/server/app.js
@@ -3,10 +3,7 @@
 var express = require('express');
 var router = express.Router();
 
-var api = require('./api')
 var config = require('./config')
-
-router.use('/api', api)
 
 router.get('*', (req, res) => {
   let clientConfig = {
