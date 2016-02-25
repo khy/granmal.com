d91ce2372338ffc338ce7fc9032bf782684f2f2d
Kevin Hyland <khy@me.com>
2016-02-24 23:29:30 -0500

    Initial work on AddUser modal for Sessions

---
+++ apps/budget/client/components/Contexts/Modal/AddUser.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import Select from 'react-select'
+import _map from 'lodash/collection/map'
+
+import { PrimaryButton, SecondaryButton } from 'client/components/bootstrap/button'
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'client/components/bootstrap/modal'
+
+export default class AddUser extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  close(event) {
+    event.preventDefault()
+    this.props.onClose()
+  }
+
+  add(event) {
+    event.preventDefault()
+    this.props.onAdd(this.state.selectedUserOption.value)
+  }
+
+  getUserOptions(input, callback) {
+    return this.props.client.get(`/accounts?handle=${input}`).then((accounts) => {
+      const options = _map(accounts, (account) => {
+        return {
+          value: account.guid,
+          label: `${account.user.handle} (${account.user.name})`,
+        }
+      })
+
+      return { options }
+    })
+  }
+
+  selectUserGuid(option) {
+    this.setState({selectedUserOption: option})
+  }
+
+  render() {
+    return (
+      <Modal>
+        <ModalHeader>Add User</ModalHeader>
+        <ModalBody>
+          <form>
+            <fieldset disabled={this.props.modalIsPosting}>
+              <fieldset className="form-group">
+                <Select.Async
+                  value={this.state.selectedUserOption}
+                  loadOptions={this.getUserOptions.bind(this)}
+                  onChange={this.selectUserGuid.bind(this)}
+                />
+              </fieldset>
+            </fieldset>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <SecondaryButton
+            onClick={this.close.bind(this)}
+            disabled={this.props.modalIsPosting}
+          >
+            Close
+          </SecondaryButton>
+
+          <PrimaryButton
+            onClick={this.add.bind(this)}
+            disabled={this.props.modalIsPosting}
+          >
+            Add
+          </PrimaryButton>
+        </ModalFooter>
+      </Modal>
+    )
+  }
+
+}
--- apps/budget/client/components/Contexts/index.jsx
+++ apps/budget/client/components/Contexts/index.jsx
@@ -3,11 +3,41 @@ import { connect } from 'react-redux'
 import _map from 'lodash/collection/map'
 
 import Navbar from '../Navbar'
+import { coreClient } from 'budget/client/lib/clients'
 import { formatDate } from 'budget/client/lib/date'
+import AddUserModal from './Modal/AddUser'
 
 class Contexts extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  showAddUserModal(event) {
+    event.preventDefault()
+    this.setState({activeModal: 'addUser'})
+  }
+
+  hideModal() {
+    this.setState({activeModal: undefined})
+  }
+
+  addUser(guid) {
+    console.log(guid)
+  }
+
   render() {
+    let modal
+
+    if (this.state.activeModal === 'addUser') {
+      modal = <AddUserModal
+        client={coreClient(this.props)}
+        onAdd={this.addUser.bind(this)}
+        onClose={this.hideModal.bind(this)}
+      />
+    }
+
     let rows
 
     if (this.props.app.contexts.length > 0) {
@@ -18,6 +48,7 @@ class Contexts extends React.Component {
             <td>{context.name}</td>
             <td>{_map(context.users, (user) => user.name).join(", ")}</td>
             <td>{formatDate(context.createdAt)}</td>
+            <td><a onClick={this.showAddUserModal.bind(this)} data-guid={context.guid} href="#">Add User</a></td>
           </tr>
         )
       })
@@ -43,6 +74,7 @@ class Contexts extends React.Component {
                 <th>Name</th>
                 <th>Users</th>
                 <th>Created At</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
@@ -50,6 +82,8 @@ class Contexts extends React.Component {
             </tbody>
           </table>
         </div>
+
+        {modal}
       </div>
     )
   }
