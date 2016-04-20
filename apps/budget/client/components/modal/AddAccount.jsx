5afecdb19b16ef5b2113f41b7f19efaf66892b22
Kevin Hyland <khy@me.com>
2016-04-19 21:33:30 -0400

    Update AddAccount modal

--- apps/budget/client/components/modal/AddAccount.jsx
+++ apps/budget/client/components/modal/AddAccount.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import _map from 'lodash/map'
 
+import { PrimaryButton, SecondaryButton } from 'client/components/bootstrap/button'
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'client/components/bootstrap/modal'
+
 export default class AddAccount extends React.Component {
 
   get isDisabled() {
@@ -34,61 +37,47 @@ export default class AddAccount extends React.Component {
     )
 
     return (
-      <div>
-        <div className="modal-backdrop in" onClick={this.close.bind(this)}></div>
-        <div className="modal" style={{display: 'block', paddingLeft: '0px'}}>
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button className="close" type="button" onClick={this.close.bind(this)}>
-                  <span>&times;</span>
-                </button>
-                <h4 className="modal-title">New Account</h4>
-              </div>
-              <div className="modal-body">
-                <form>
-                  <fieldset disabled={this.isDisabled}>
-                    <fieldset className="form-group">
-                      <label>Account Type</label>
-                      <select ref="accountTypeGuidSelect" className="form-control">
-                        {accountTypeOptions}
-                      </select>
-                    </fieldset>
+      <Modal>
+        <ModalHeader>New Account</ModalHeader>
+        <form onSubmit={this.add.bind(this)}>
+          <ModalBody>
+            <fieldset disabled={this.isDisabled}>
+              <fieldset className="form-group">
+                <label>Account Type</label>
+                <select ref="accountTypeGuidSelect" className="form-control">
+                  {accountTypeOptions}
+                </select>
+              </fieldset>
 
-                    <fieldset className="form-group">
-                      <label>Name</label>
-                      <input ref="nameInput" className="form-control" type="text" />
-                    </fieldset>
+              <fieldset className="form-group">
+                <label>Name</label>
+                <input ref="nameInput" className="form-control" type="text" />
+              </fieldset>
 
-                    <fieldset className="form-group">
-                      <label>Intial Balance</label>
-                      <input ref="initialBalanceInput" className="form-control" type="text" />
-                    </fieldset>
-                  </fieldset>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={this.close.bind(this)}
-                  disabled={this.isDisabled}
-                >
-                  Close
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={this.add.bind(this)}
-                  disabled={this.isDisabled}
-                >
-                  Add
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              <fieldset className="form-group">
+                <label>Intial Balance</label>
+                <input ref="initialBalanceInput" className="form-control" type="text" />
+              </fieldset>
+            </fieldset>
+          </ModalBody>
+          <ModalFooter>
+            <SecondaryButton
+              onClick={this.close.bind(this)}
+              disabled={this.isDisabled}
+            >
+              Close
+            </SecondaryButton>
+
+            <PrimaryButton
+              type="submit"
+              onClick={this.add.bind(this)}
+              disabled={this.isDisabled}
+            >
+              Add
+            </PrimaryButton>
+          </ModalFooter>
+        </form>
+      </Modal>
     )
   }
 
