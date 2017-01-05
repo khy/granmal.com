c5086c605f8365b20a83c351863012e1eb7d62fe
Kevin Hyland <kevin@evenfinancial.com>
2017-01-04 22:25:02 -0500

    Add variables to NewMovement in fran

--- apps/fran/client/components/NewMovement.jsx
+++ apps/fran/client/components/NewMovement.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import Select from 'react-select'
 
+import { Icon } from 'client/components/fontAwesome'
 import { FormModal } from 'client/components/bootstrap/modal'
 import { FormGroup, TextInput } from 'client/components/bootstrap/form'
+import { SecondaryButton } from 'client/components/bootstrap/button'
 
 export default class NewMovement extends React.Component {
 
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      variables: []
+    }
   }
 
   addMovement() {
     this.props.onAdd(this.state)
+  }
+
+  addVariable() {
+    const oldVariables = this.state.variables
+    this.setState({
+      variables: [...oldVariables, {}]
+    })
   }
 
   setAttribute(key, event) {
@@ -22,7 +33,52 @@ export default class NewMovement extends React.Component {
     this.setState(newState)
   }
 
+  setVariableName(index, event) {
+    this.setVariableAttribute(index, 'name', event.target.value)
+  }
+
+  setVariableDimension(index, option) {
+    this.setVariableAttribute(index, 'dimension', option.value)
+  }
+
+  setVariableAttribute(index, key, value) {
+    this.setState((prevState) => {
+      let variables = prevState.variables.slice()
+      let variable = variables[index] || {}
+      variable[key] = value
+      variables[index] = variable
+      return variables
+    })
+  }
+
   render() {
+    const variableFields = this.state.variables.map((variable, index) => {
+      return (
+        <div className='row' key={`variable${index}`}>
+          <div className='col-sm-6'>
+            <FormGroup>
+              <TextInput
+                onChange={this.setVariableName.bind(this, index)}
+                disabled={this.props.disabled}
+                placeholder="Variable Name"
+              />
+            </FormGroup>
+          </div>
+          <div className='col-sm-6'>
+            <FormGroup>
+              <Select
+                placeholder='Dimension'
+                value={variable.dimension || ''}
+                onChange={this.setVariableDimension.bind(this, index)}
+                options={this.props.dimensionOptions}
+              />
+            </FormGroup>
+          </div>
+        </div>
+      )
+      return <div>Variable!</div>
+    })
+
     return (
       <FormModal
         title='New Movement'
@@ -39,6 +95,11 @@ export default class NewMovement extends React.Component {
             disabled={this.props.disabled}
           />
         </FormGroup>
+        {variableFields}
+        <SecondaryButton onClick={this.addVariable.bind(this)}>
+          <Icon name="plus" />
+          <span> Add Variable</span>
+        </SecondaryButton>
       </FormModal>
     )
   }
@@ -46,11 +107,15 @@ export default class NewMovement extends React.Component {
 }
 
 NewMovement.propTypes = {
+  dimensionOptions: React.PropTypes.arrayOf(React.PropTypes.object),
   disabled: React.PropTypes.bool,
   onAdd: React.PropTypes.func.isRequired,
   onClose: React.PropTypes.func.isRequired,
 }
 
 NewMovement.defaultProps = {
+  dimensionOptions: [
+    { value: 'weight', label: 'Weight' },
+  ],
   disabled: false,
 }
